889e506d4ad9ed7710a1100c79d8c8c91f6d11cc
real-casey-james <caseysjames@gmail.com>
2021-05-29 12:40:52 +1200

    bowling scorer works

--- bowling/score.js
+++ bowling/score.js
@@ -1,17 +1,37 @@
-let game = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0, 0]]
+let game = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0, 0], [0,0],[0,0]]
+
+let balls = [['ball1', 'ball2'], ['ball3', 'ball4'], ['ball5', 'ball6'],['ball7', 'ball8'], ['ball9','ball10'], ['ball11', 'ball12'],['ball13', 'ball14'], ['ball15','ball16'], ['ball17', 'ball18'],['ball19', 'ball20', 'ball21']]
 
 document.addEventListener("click", () => updateFrames(game))
 
-console.log('working')
-
 function updateFrames (game) {
   //loop through each frame, get ball by id, update game array
-  for (var i = 1; i < 18; i+=2) {
-    game[i][0] = document.getElementById("ball" + i).value
-    game[i][1] = document.getElementById("ball" + i + 1).value
+  for (var i = 0; i < 10; i++) {
+    game[i][0] = Number(document.getElementById(balls[i][0]).value)
+    game[i][1] = Number(document.getElementById(balls[i][1]).value)
   }
+  //handle 3rd ball in 10th frame
+  game[9][2] = Number(document.getElementById(balls[9][2]).value)
   //run scoregame and update total
-  document.getElementById('score').value = scoreGame(game)
+  document.getElementById('score').innerHTML = scoreGame(game)
+  //update max score for balls
+  updateMax(game)
+}
+
+function updateMax (game) {
+  for (var i = 0; i < 9; i++) {
+    document.getElementById(balls[i][0]).max = 10 - game[i][1]
+    document.getElementById(balls[i][1]).max = 10 - game[i][0]
+
+    if (game[i][0] == 10) {
+      document.getElementById(balls[i][1]).style.visibility = "hidden"
+      
+      
+      //sound effect
+    } else {
+      document.getElementById(balls[i][1]).style.visibility = "visible"
+    }
+  }
 }
 
 
@@ -37,7 +57,6 @@ function scoreDoubleStrike(frames) {
 
 function scoreGame(frames) {
   let score = 0
-  frames.push([0,0],[0,0])
   score += frames.flat()[20]
   for (let x = 0; x < frames.length; x++) {
     //adds points for non strikes  
