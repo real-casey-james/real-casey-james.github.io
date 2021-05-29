34347fbf6204081cd3be388e42de6ff5f31f7dd1
real-casey-james <caseysjames@gmail.com>
2021-05-29 19:50:25 +1200

    finished ascii viewer

--- ascii/index.js
+++ ascii/index.js
@@ -1,97 +1,30 @@
+let data = {}
 
-$.getJSON('./data', data => {
-  console.log(data); //["doc1.jpg", "doc2.jpg", "doc3.jpg"] 
-});
+function fetcher () {(async () => {
+  const response = await fetch('https://api.github.com/repos/real-casey-james/real-casey-james.github.io/contents/ascii/data');
+  data = await response.json();
+  console.log(data)
+  let htmlString = '';
+  for (let file of data) {
+    htmlString += `<button onclick="getFile('${file.path.slice(5)}')">${file.name.slice(0,-4)}</button>`;
+  }
 
+  document.getElementById('buttonContainer').outerHTML = htmlString;
+})()
+}
 
-// // const fs = require('fs')
-// // const readline = require('readline')
-
-// // const rl = readline.createInterface({
-// //   input: process.stdin,
-// //   output: process.stdout
-// // })
-
-// function fetcher() {
-//   var generateButtons = ""
-//   fs.readdir("./data", function(files) {
-//     for (let i = 0; i < files.length; i++) {
-//       generateButtons += "<button>" + files[i] + "</button>"
-//     }
-//   })
-//   document.getElementById("buttonContainer").innerHTML = generateButtons
-// }
-
-// function mainMenu () {
-//   console.log('Morena')
-//   console.log('------')
-//   console.log('Select artwork, or another function')
-//   console.log('"c" to comment')
-//   console.log('"v" to view comments')
-//   console.log('"e" to erase comments')
-//   console.log('"q" to quit \n')
+function getFile (path) {
   
-//   fs.readdir("./data", function(err, files) {
-//     for (let i = 0; i < files.length; i++) {
-//       print((i + 1 +  ': ') + files[i])
-//     }
-  
-//   rl.question('Make your selection: \n', function (input) {
-    
-// if (input >= 1 && input <= 5) {
-//     fs.readFile(__dirname + '/data/' + files[input], 'utf8',(err,data) => {
-//       print(data)
-//     })
-//   } else if (input == 'q') {
-//     process.exit()
-//   } else if (input == 'c') {
-//     comment()
-//   } else if (input == 'v') {
-//     readComment()
-//   } else if (input == 'e') {
-//     erase()
-//   }
-//   })
-// })
-// }
+fetch(path)
+  .then(function(response) {
+    response.text().then(function(text) {
+      storedText = text;
+      document.getElementById("artContainer").innerHTML = text
+    });
+  });
+}
 
-// function comment () {
-//   rl.question('What do you wanna say?', (answer) => {
-//     rl.close()
-    
-//     fs.writeFile('comment.txt', answer, (err) => {
-     
-//       if (err)
-//         console.log(err);
-//       else {
-//         console.log("File written successfully\n");
-//         console.log("The written has the following contents:");
-//         console.log(fs.readFileSync("comment.txt", "utf8"));
-//     }
-//   })
-// })
-// }
-
-
-// function readComment() {
-//   rl.close()
-//   console.log(fs.readFileSync("comment.txt", "utf8"));
-// }
-
-// function erase() {
-//   rl.close()
-//   fs.writeFile('comment.txt', "", function(){console.log('done')})
-// }
-
-// // mainMenu()
-
-
-// // function print (stuff) {
-// //   console.log(stuff)
-// // }
-
-// //TODO - fs.appendFile
-
-// module.exports = {
-// //TODO
-// }
+function changeColor() {
+  document.body.style.color = document.getElementById("textColor").value;
+  document.body.style.backgroundColor = document.getElementById("backgroundColor").value;
+}
--- ascii/index.test.js
+++
@@ -1,5 +0,0 @@
-const birds = require('./index.js')
-
-test('test setup working', () => {
-  expect(true).toBeTruthy()
-})
